6a62de0283d5daf32ff60c3a1079ee5648d299e1
mohamadmahdi-87 <mohamadmahditajkarimi@gmail.com>
2024-11-07 13:45:52 +0330

    alert changes

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,42 @@
 <template>
 	<div class="flex items-center justify-center overflow-hidden min-h-screen bg-[#080710]">
-		<div class="z-1 relative w-[400px]">
-			<div class="absolute top-[-80px] left-[-80px] w-[200px] h-[200px] rounded-full bg-gradient-to-r from-[#0D47A1] to-[#42A5F5] z-[-1]"></div>
-			<div class="absolute bottom-[-90px] right-[-60px] w-[200px] h-[200px] rounded-full bg-gradient-to-r from-[#E65100] to-[#FFA726] z-[-1]"></div>
-
-			<form @submit.prevent="SubmitEvent" action="#" class="bg-white/10 p-[35px] pt-[50px] border border-white/10 rounded-[10px] shadow-[0_0_40px_rgba(0,0,0,0.5)] backdrop-blur text-white">
-				<h2 class="text-4xl text-center mb-8 font-semibold">Login Here</h2>
-
-				<div class="mb-6">
-					<label class="block text-lg mb-2" for="username">Username</label>
-					<input
-						type="text"
-						v-model="name"
-						id="username"
-						placeholder="username"
-						required
-						class="w-full h-[50px] bg-white/10 px-3 text-white placeholder:text-gray-300 rounded-[4px] focus:outline-none" />
-				</div>
-
-				<div class="mb-6">
-					<label class="block text-lg mb-2" for="email">email</label>
-					<input
-						type="email"
-						v-model="email"
-						id="email"
-						placeholder="email"
-						required
-						class="w-full h-[50px] bg-white/10 px-3 text-white placeholder:text-gray-300 rounded-[4px] focus:outline-none" />
-				</div>
-
-				<button type="submit" class="w-full py-4 mt-10 bg-white text-[#080710] rounded-[4px] font-bold text-lg">Log In</button>
-				<ResultAlert v-if="submitted" :name="name" :email="email" />
-			</form>
+		<div class="z-1 p-0.5 overflow-hidden rounded-[10px] relative w-[400px]" @mousemove="handleMouseMove" @mouseleave="hideCircle">
+			<div class="circle -z-1" :style="{top: `${y - 200}px`, left: `${x - 200}px`, opacity: isCircleVisible ? 0.6 : 0}"></div>
+			<div class="bg-black z-10 rounded-[10px]">
+				<form
+					@submit.prevent="SubmitEvent"
+					class="overflow-hidden bg-white/10 z-10 relative p-[35px] pt-[50px] border border-white/10 rounded-[10px] shadow-[0_0_40px_rgba(0,0,0,0.5)] backdrop-blur text-white">
+					<h2 class="text-4xl text-center mb-8 font-semibold">Login Here</h2>
+					<div class="mb-6">
+						<label class="block text-lg mb-2" for="username">Username</label>
+						<input
+							type="text"
+							v-model="name"
+							id="username"
+							placeholder="username"
+							required
+							class="w-full h-[50px] bg-white/10 px-3 text-white placeholder:text-gray-300 rounded-[4px] focus:outline-none" />
+					</div>
+					<div class="mb-6">
+						<label class="block text-lg mb-2" for="email">Email</label>
+						<input
+							type="email"
+							v-model="email"
+							id="email"
+							placeholder="email"
+							required
+							class="w-full h-[50px] bg-white/10 px-3 text-white placeholder:text-gray-300 rounded-[4px] focus:outline-none" />
+					</div>
+					<button type="submit" class="w-full py-4 mt-10 bg-white text-[#080710] rounded-[4px] font-bold text-lg">Log In</button>
+				</form>
+			</div>
 		</div>
 	</div>
+	<ResultAlert v-if="submitted" :name="name" :email="email" />
 </template>
-
 <script>
 import ResultAlert from "./components/ResultAlert.vue";
+import {ref} from "vue";
 
 export default {
 	components: {
@@ -53,8 +52,33 @@ export default {
 	methods: {
 		SubmitEvent() {
 			this.submitted = true;
-			console.log("submitted");
 		},
+	},
+	setup() {
+		const x = ref(0);
+		const y = ref(0);
+		const isCircleVisible = ref(false);
+
+		// تغییر مکان دایره با حرکت موس
+		const handleMouseMove = (event) => {
+			const rect = event.currentTarget.getBoundingClientRect();
+			x.value = event.clientX - rect.left;
+			y.value = event.clientY - rect.top;
+			isCircleVisible.value = true;
+		};
+
+		// مخفی‌سازی دایره وقتی موس خارج شد
+		const hideCircle = () => {
+			isCircleVisible.value = false;
+		};
+
+		return {
+			x,
+			y,
+			isCircleVisible,
+			handleMouseMove,
+			hideCircle,
+		};
 	},
 };
 </script>
--- src/components/ResultAlert.vue
+++ src/components/ResultAlert.vue
@@ -1,13 +1,27 @@
 <template>
-	<div v-if="showAlert" class="fixed top-5 p-6 bg-neutral-600 border-r-4 border-green-600 text-white rounded-l-md shadow-lg transition-all duration-500 ease-out" :class="alertClass">
-		<p>
-			<strong>Name:</strong>
-			{{ name }}
-		</p>
-		<p>
-			<strong>Email:</strong>
-			{{ email }}
-		</p>
+	<div
+		@mouseenter="stopTimer"
+		@mouseleave="resumeTimer"
+		v-if="showAlert"
+		:class="alertClass"
+		class="fixed flex items-center max-w-110 gap-7 top-5 p-6 bg-neutral-600 border-r-4 border-green-600 text-white rounded-l-md shadow-lg transition-all duration-500 ease-out">
+		<div @click="hideAlert" class="transition-all group hover:text-red-500 flex place-content-center">
+			<span class="w-10 h-10 flex justify-center items-center bg-neutral-800 rounded-full">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="group-hover:scale-[1.1] size-7">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+				</svg>
+			</span>
+		</div>
+		<div>
+			<p>
+				<strong>Name:</strong>
+				{{ name }}
+			</p>
+			<p>
+				<strong>Email:</strong>
+				{{ email }}
+			</p>
+		</div>
 	</div>
 </template>
 
@@ -18,15 +32,34 @@ export default {
 		return {
 			showAlert: true,
 			alertClass: "translate-x-full right-0",
+			isTiming: null,
 		};
 	},
 	mounted() {
-		setTimeout(() => {
-			this.alertClass = "translate-x-0 right-6";
-		}, 50);
-		setTimeout(() => {
+		this.timing();
+	},
+	methods: {
+		timing() {
+			setTimeout(() => {
+				this.alertClass = "translate-x-0 right-6";
+			}, 50);
+
+			this.isTiming = setTimeout(() => {
+				this.hideAlert();
+			}, 2500);
+		},
+		stopTimer() {
+			clearTimeout(this.isTiming);
+		},
+
+		resumeTimer() {
+			this.isTiming = setTimeout(() => {
+				this.hideAlert();
+			}, 2500);
+		},
+		hideAlert() {
 			this.alertClass = "translate-x-full right-0";
-		}, 2500);
+		},
 	},
 };
 </script>
